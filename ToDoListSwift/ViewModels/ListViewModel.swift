0461734b4c359483468651b20a763d05cb859bbc
Nortiz <nortiz1349@gmail.com>
2022-10-11 16:32:49 +0900

    save and persist with UserDefaults

--- ToDoListSwift/ViewModels/ListViewModel.swift
+++ ToDoListSwift/ViewModels/ListViewModel.swift
@@ -20,19 +20,25 @@ import Foundation
 
 class ListViewModel: ObservableObject {
 	
-	@Published var items: [ItemModel] = []
+	@Published var items: [ItemModel] = [] {
+		didSet {
+			saveItem()
+		}
+	}
+	
+	let itemsKey: String = "item_list"
 	
 	init() {
 		getItems()
 	}
 	
 	func getItems() {
-		let newItems = [
-			ItemModel(title: "첫번째 아이템", isCompleted: false),
-			ItemModel(title: "두번째 아이템", isCompleted: true),
-			ItemModel(title: "세번째 아이템", isCompleted: false)
-		]
-		items.append(contentsOf: newItems)
+		guard
+			let data = UserDefaults.standard.data(forKey: itemsKey),
+			let savedItems = try? JSONDecoder().decode([ItemModel].self, from: data)
+		else { return }
+		
+		self.items = savedItems
 	}
 	
 	func deleteItem(indexSet: IndexSet) {
@@ -51,6 +57,12 @@ class ListViewModel: ObservableObject {
 	func updateItem(item: ItemModel) {
 		if let index = items.firstIndex(where: { $0.id == item.id }) {
 			items[index] = item.updateCompletion()
+		}
+	}
+	
+	func saveItem() {
+		if let encodedData = try? JSONEncoder().encode(items) {
+			UserDefaults.standard.set(encodedData, forKey: itemsKey)
 		}
 	}
 }
