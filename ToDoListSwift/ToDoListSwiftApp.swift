8f6d8f757ce9d5cd11214f4bdd1d1e7cd047522e
Nortiz <nortiz1349@gmail.com>
2022-10-11 11:06:41 +0900

    Initial Commit

---
+++ ToDoListSwift/ToDoListSwiftApp.swift
@@ -0,0 +1,17 @@
+//
+//  ToDoListSwiftApp.swift
+//  ToDoListSwift
+//
+//  Created by Nortiz M1 on 2022/10/11.
+//
+
+import SwiftUI
+
+@main
+struct ToDoListSwiftApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
